1836633790e194383e26a0ce9850a962f21a2f65
Hilary <g33klady@gmail.com>
2019-10-20 16:44:36 -0400

    Lesson 5 and 6 - added new project, installed NuGet packages, and created first check

---
+++ ApiChecks/ApiChecksClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Net;
+
+namespace ApiChecks
+{
+    [TestFixture]
+    public class ApiChecksClass
+    {
+        [Test]
+        public void VerifyGetAllTodoItemsReturns200()
+        {
+            //Arrange
+            var client = new RestClient("https://localhost:44367/api/Todo");
+            var request = new RestRequest(Method.GET);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, $"GET all todo items did not return a success status code; it returned {response.StatusCode}");
+        }
+    }
+}
