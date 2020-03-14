889f80ca2a90f153e4e7871cac6e3808ccfacdc3
Hilary <g33klady@gmail.com>
2020-03-14 12:41:23 -0400

    Lesson 7 - added check to verify name of first todo item using deserialization

--- ApiChecks/ApiChecksClass.cs
+++ ApiChecks/ApiChecksClass.cs
@@ -1,6 +1,8 @@
-﻿using NUnit.Framework;
+﻿using APIChecksWithRestSharp.Models;
+using NUnit.Framework;
 using RestSharp;
 using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace ApiChecks
@@ -21,5 +23,22 @@ namespace ApiChecks
             //Assert
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, $"GET all todo items did not return a success status code; it returned {response.StatusCode}");
         }
+
+        [Test]
+        public void VerifyGetItem1ReturnsNameWalkTheDog()
+        {
+            //TODO: Refactor - this Arrange is not DRY
+            //Arrange
+            var client = new RestClient("https://localhost:44367/api/Todo/1");
+            var request = new RestRequest(Method.GET);
+
+            //Act
+            IRestResponse<TodoItem> todoItem = client.Execute<TodoItem>(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, todoItem.StatusCode, $"GET todo item 1 did not return a success status code; it returned {todoItem.StatusCode}");
+            StringAssert.AreEqualIgnoringCase("Walk the Dog", todoItem.Data.Name, $"Actual name should have been 'walk the dog' but it was {todoItem.Data.Name}"); //This is fragile! 
+        }
+
     }
 }
